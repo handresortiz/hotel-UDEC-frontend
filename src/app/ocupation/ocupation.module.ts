3237bf8ecd718404891dca51d6676d104856445b
Daniel-Cas <dmattcomics@gmail.com>
2021-10-30 12:04:11 -0500

    Skeleton created for develop by modules

---
+++ src/app/ocupation/ocupation.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OcupationRoutingModule } from './ocupation-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OcupationRoutingModule
+  ]
+})
+export class OcupationModule { }
