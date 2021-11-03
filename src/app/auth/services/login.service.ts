131abb03ad091d7ec676af446ae14a25e9eaad94
Daniel-Cas <dmattcomics@gmail.com>
2021-11-03 11:41:48 -0500

    Path about microservices added

---
+++ src/app/auth/services/login.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  private url: String = environment.microserviceLogin;
+
+  constructor() { }
+}
