34423c7ef490eeed65e080f260d41afb6dc5a4a6
SebastianPacMor <sebitasp002@gmail.com>
2021-11-05 23:11:25 -0500

    Galeria de imagenes añadida

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -4,12 +4,18 @@ import { CommonModule } from '@angular/common';
 import { ReservationRoutingModule } from './reservation-routing.module';
 import { DetallesHabitacionComponent } from './components/detalles-habitacion/detalles-habitacion.component';
 
+//Galeria de imágenes
+import { GalleriaModule } from 'primeng/galleria';
+import { PhotoService } from './services/photoservice';
+
 
 @NgModule({
   declarations: [DetallesHabitacionComponent],
   imports: [
     CommonModule,
+    GalleriaModule,
     ReservationRoutingModule
-  ]
+  ],
+  providers: [PhotoService]
 })
 export class ReservationModule { }
