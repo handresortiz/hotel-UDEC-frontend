ee4b8805acf806e6bbe5a09c3991c13e52fb5394
Sebastian Vicaña <junsevicabarrios@gmail.com>
2021-11-06 20:00:41 -0500

    Panel principal de reservación V1.1 beta

---
+++ src/app/reservation/components/reservar/reservar.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reservar',
+  templateUrl: './reservar.component.html',
+  styleUrls: ['./reservar.component.scss']
+})
+
+export class ReservarComponent implements OnInit {
+
+//aqui poner los atributos xD
+fec_inicio= new Date() ;
+fec_fin= new Date() ;
+num_adultos: number=0;
+num_ninos: number=0;
+handleClick() {
+  //execute action
+}
+
+  constructor() {
+   }
+
+  ngOnInit(): void {
+  }
+
+}
