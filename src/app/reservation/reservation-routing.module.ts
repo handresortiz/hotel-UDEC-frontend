1e9319d0d33409b88f61dd9c9a30f4b85871bd29
Sebastian Vicaña <junsevicabarrios@gmail.com>
2021-11-06 19:20:15 -0500

    Auto stash before merge of "Developer---Reservas" and "origin/Developer---Reservas"
    Panel principal de reservación V1.0 beta

--- src/app/reservation/reservation-routing.module.ts
+++ src/app/reservation/reservation-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DetallesHabitacionComponent } from './components/detalles-habitacion/detalles-habitacion.component';
+<<<<<<< Updated upstream
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
 
 const routes: Routes = [
   {path: '', children: [
     {path: 'detalle-habitacion', component: DetallesHabitacionComponent},
     {path: 'catalogo', component: CatalogoComponent}
+=======
+import { ReservarComponent } from './components/reservar/reservar.component';
+const routes: Routes = [
+  {path: '', children: [
+    {path: 'detalle-habitacion', component: DetallesHabitacionComponent},
+    {path: 'reserve-ahora', component:ReservarComponent }
+>>>>>>> Stashed changes
   ]}
 ];
 
--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReservarComponent } from './components/reservar/reservar.component';
 import { ReservationRoutingModule } from './reservation-routing.module';
 import { DetallesHabitacionComponent } from './components/detalles-habitacion/detalles-habitacion.component';
-
 //Galeria de imágenes
 import { GalleriaModule } from 'primeng/galleria';
 import { PhotoService } from './services/photoservice';
@@ -13,18 +13,28 @@ import { ButtonModule } from 'primeng/button';
 import { DynamicDialogModule } from 'primeng/dynamicdialog';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { DialogService } from 'primeng/dynamicdialog';
+//Reservar
+import {CalendarModule} from 'primeng/calendar';
+import{ FormsModule } from '@angular/forms';
+import {InputNumberModule} from 'primeng/inputnumber';
 
 @NgModule({
   declarations: [
     DetallesHabitacionComponent,
     CatalogoComponent,
+    ReservarComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
     GalleriaModule,
     ButtonModule,
     DynamicDialogModule,
-    ReservationRoutingModule
+    ReservationRoutingModule,
+    CalendarModule,
+    InputNumberModule,
+    ButtonModule
+
   ],
   providers: [
     PhotoService,
