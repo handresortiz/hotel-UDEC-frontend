215951b87176f9a9ffdb1bfebe06082f0bad9188
Sebastian Vicaña <junsevicabarrios@gmail.com>
2021-11-25 03:50:10 -0500

    Creación interface habitacion
    interfaces para crear, modificar y/o eliminar una habitación

--- src/app/parametrics/parametrics-routing.module.ts
+++ src/app/parametrics/parametrics-routing.module.ts
@@ -3,14 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { TiposhabitacionesComponent } from './components/tiposhabitaciones/tiposhabitaciones.component';
 import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component'
-
+import { ListarHabitacionesComponent } from './components/listar-habitaciones/listar-habitaciones.component'
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: 'tipos-habitaciones', component: TiposhabitacionesComponent },
       { path: 'habitaciones', component: HabitacionesComponent },
-      { path: 'usuarios', component: UsuariosComponent }
+      { path: 'usuarios', component: UsuariosComponent },
+      { path: 'listar-habitaciones', component: ListarHabitacionesComponent }
     ]
   }
 ];
