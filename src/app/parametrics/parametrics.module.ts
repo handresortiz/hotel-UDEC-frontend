5a8b5792bb7564566e9108fb2a51675fd61b5b24
SebastianPacMor <sebitasp002@gmail.com>
2021-11-25 00:18:16 -0500

    Finalización interfaz para tipos de habitaciones
    Conclusión de la interfaz con los métodos, servicios, muestra, adición y edición de los tipos de habitaciones

--- src/app/parametrics/parametrics.module.ts
+++ src/app/parametrics/parametrics.module.ts
@@ -6,6 +6,31 @@ import { TiposhabitacionesComponent } from './components/tiposhabitaciones/tipos
 import { HabitacionesComponent } from './components/habitaciones/habitaciones.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
 
+//Tabla para los tipos de habitación
+import {FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { TiposService } from './services/TiposService';
+import {TableModule} from 'primeng/table';
+import {ToastModule} from 'primeng/toast';
+import {CalendarModule} from 'primeng/calendar';
+import {SliderModule} from 'primeng/slider';
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ContextMenuModule} from 'primeng/contextmenu';
+import {DialogModule} from 'primeng/dialog';
+import {ButtonModule} from 'primeng/button';
+import {DropdownModule} from 'primeng/dropdown';
+import {ProgressBarModule} from 'primeng/progressbar';
+import {InputTextModule} from 'primeng/inputtext';
+import {FileUploadModule} from 'primeng/fileupload';
+import {ToolbarModule} from 'primeng/toolbar';
+import {RatingModule} from 'primeng/rating';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputNumberModule} from 'primeng/inputnumber';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { MessageService } from 'primeng/api';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +40,28 @@ import { UsuariosComponent } from './components/usuarios/usuarios.component';
   ],
   imports: [
     CommonModule,
-    ParametricsRoutingModule
-  ]
+    ParametricsRoutingModule,
+    TableModule,
+    CalendarModule,
+		SliderModule,
+		DialogModule,
+		MultiSelectModule,
+		ContextMenuModule,
+		DropdownModule,
+		ButtonModule,
+		ToastModule,
+    InputTextModule,
+    ProgressBarModule,
+    HttpClientModule,
+    FileUploadModule,
+    ToolbarModule,
+    RatingModule,
+    FormsModule,
+    RadioButtonModule,
+    InputNumberModule,
+    ConfirmDialogModule,
+    InputTextareaModule,
+  ],
+  providers: [TiposService, MessageService, ConfirmationService]
 })
 export class ParametricsModule { }
